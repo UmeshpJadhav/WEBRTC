6b60ff9a14dc77e355d97921de7b51b8746cae8f
UmeshpJadhav <Umesh2005jadhav@gmail.com>
2025-03-06 18:22:35 +0530

    main

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require('path');
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Use environment variable for port or default to 3000
+const PORT = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -15,11 +18,17 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on("signalingMessage", (message) => {  
-      socket.broadcast.emit("signalingMessage", message);
+    console.log('A user connected');
+    
+    socket.on("signalingMessage", (message) => {
+        socket.broadcast.emit("signalingMessage", message);
     });
-  });
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
